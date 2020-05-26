ab3a57f344a0db1ed6607bdfac123dc210464933
nikitasattarov <59117720+nikitasattarov@users.noreply.github.com>
2020-05-26 18:57:56 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <locale.h>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand ( time(NULL) );
+    setlocale(0, "Russian");
+    int i, n, random_int;
+    cout << "Введите длину строки: " << endl;
+    cin >> n;
+    cout << "Программа считает, сколько раз меняется знак в массиве." << endl;
+    cout << "Строка со случайными значениями: " << endl;
+    vector<int> m;
+    int val;
+    for (i = 0; i < n; i++){
+        random_int = rand() % 30 - 15;
+        cout << random_int << " ";
+        m.push_back(random_int);
+    }
+    cout << endl;
+
+
+vector<int>::iterator iter = m.begin();
+    int k = 0;
+    int a, b;
+    while (iter + 1 != m.end())
+    {
+        a = *iter;
+        iter++;
+        b = *iter;
+        if (a == 0){
+            if (b < 0) k += 1;
+        }
+        if (b == 0){
+            if (a < 0) k += 1;
+        }
+        if ((a != 0) && (b != 0)){
+            if (a * b < 0) k += 1;
+        }
+    }
+    cout << "Количество смен знака в массиве: ";
+    cout << k;
+    return 0;
+}
